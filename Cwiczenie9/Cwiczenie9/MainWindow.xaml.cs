e890b331b4c8d45a96d4f1522f7924b6135f02fd
Defler <48053668+Defler@users.noreply.github.com>
2019-12-04 15:52:32 +0100

    Add files via upload

---
+++ Cwiczenie9/Cwiczenie9/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cwiczenie9
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        string imie;
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            imie = MyTextBoxPobieranie.Text;
+            if (MyTextBoxPobieranie.Text == "[wprowadz imie]")
+                MyTextBoxWyswietlanie.Text = "Du bist debilen!";
+            else
+                MyTextBoxWyswietlanie.Text = "Witaj, Bot " + imie;
+        }
+
+        private void ButtonCzyszczenie_Click(object sender, RoutedEventArgs e)
+        {
+            MyTextBoxWyswietlanie.Text = " ";
+        }
+    }
+}
